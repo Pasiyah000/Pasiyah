a4b495aeb3d2efdd7a0f0c0b04d4dc91631b0589
Shanuwa26 <navodnavod659@gmail.com>
2024-11-11 18:34:35 +0530

    Update config-db.js

--- config-db.js
+++ config-db.js
@@ -1,7 +1,7 @@
 
-const GITHUB_AUTH_TOKEN = ""
-const GITHUB_USERNAME = ""
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const GITHUB_USERNAME = "shanuwa26"
+const SESSION_ID = "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=jihT2BCA#QWlFCsCnq5nrO_R-40y8-ulgkZByhCuevvp7w1jeyIM"
 
 
 module.exports = {
